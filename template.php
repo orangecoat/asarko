0ec7c7e4062da3f628f93034e7c51138e3e19b59
Rick Harris <rick.c.harris@gmail.com>
2011-01-31 15:24:43 -0600

    Add new page template

--- template.php
+++ template.php
@@ -3,3 +3,7 @@
 function asarko_preprocess_html(&$vars) {
   $vars['classes_array'][] = 'asarko';
 }
+
+function asarko_preprocess_page(&$vars) {
+  $vars['content_tag'] = isset($vars['node']) ? 'article' : 'section';
+}
