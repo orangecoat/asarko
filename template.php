881b997109ed878e4e742aaf313faa4ac41a20a8
Rick Harris <rick.c.harris@gmail.com>
2011-02-08 15:07:42 -0500

    Fixed a crappy modernizr implementation

--- template.php
+++ template.php
@@ -2,25 +2,25 @@
 // $Id$
 
 function asarko_theme($existing, $type, $theme, $path) {
- return array(); 
+  return array(); 
 }
 
 function asarko_preprocess_block(&$vars) {
-  
+
   $attr['id'] = "block-{$vars['block']->module}-{$vars['block']->delta}";
   $attr['class'] = 'block';
   $attr['class'] .= " block-{$vars['block']->module}";
   $attr['class'] .= " block-{$vars['block']->region}";
-  
+
   $vars['attr'] = $attr;
   $vars['empty'] = empty($vars['block']->content);
-  
+
 }
 
 function asarko_preprocess_node(&$vars) {
-  
+
   $attr['id'] = "node-{$vars['node']->nid}";
-  
+
   $attr['class'] = "node";
   if ($vars['page']) {
     $attr['class'] .= ' page';
@@ -34,35 +34,37 @@ function asarko_preprocess_node(&$vars) {
     $attr['class'] .= ' node-unpublished';
   }
   $attr['class'] .= ' clear-block';
-  
+
   $vars['attr'] = $attr;
   $vars['empty'] = empty($vars['node']->content);
-  
+
 }
 
 function asarko_preprocess_page(&$vars) {
-  
+
   // Add modernizr after stylesheets
-  $vars['styles'] .= drupal_set_html_head('<script src="' . base_path() . drupal_get_path('asarko') .'/js/modernizr.min.js"></script>');
-  
+  $vars['styles'] .= '<script src="' . base_path() .
+    drupal_get_path('theme', 'asarko') . '/js/modernizr.min.js"></script>'.
+    "\n";
+
   // If the page is a node page, we want the title to be inside the article tag
   if ($vars['node']) {
     $vars['content_tag'] = 'article';
   } else {
     $vars['content_tag'] = 'section';
   }
-  
+
   $attr['class'] = $vars['body_classes'] . ' asarko';
   $vars['attr'] = $attr;
-  
+
 }
 
 function asarko_preprocess_comment(&$vars) {
-  
+
   $attr['class'] =  'comment';
   $attr['class'] .= $comment->new ? ' comment-new' : '';
   $attr['class'] .= ' clear-block';
-  
+
   $vars['attr'] = $attr;
-  
+
 }
